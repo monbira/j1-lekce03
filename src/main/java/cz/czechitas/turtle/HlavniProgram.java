dc14e30defba5157f2fc007cbfebc5c638316ee5
monikabiras <monikabiras@gmail.com>
2024-09-15 16:53:19 +0200

    zofka kreslí zelené čtverce

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,11 +2,29 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
     private Turtle zofka;
 
     public void start() {
+
         zofka = new Turtle();
+        this.ctverec(100);
+
+        zofka.setLocation(300,400);
+        this.ctverec(150);
+
+    }
+
+
+    public void ctverec (double delkaStrany) {
+        zofka.setPenColor(Color.green);
+        for (int i = 0; i < 4; i++) {
+            zofka.move(delkaStrany);
+            zofka.turnRight(90);
+        }
+
     }
 
     public static void main(String[] args) {
